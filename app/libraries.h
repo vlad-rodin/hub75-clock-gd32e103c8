f7446b63621e47d307bf420d240b95a76f008b6f
Vlad (Desktop) <tularodin@mail.ru>
2023-03-19 15:47:07 +0300

    initial

---
+++ app/libraries.h
@@ -0,0 +1,12 @@
+#ifndef LIBRAIES_H_
+#define LIBRAIES_H_
+
+#include <inttypes.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "gd32e10x.h"
+
+#endif // LIBRAIES_H_
---
+++ app/main.c
@@ -0,0 +1,13 @@
+#include "libraries.h"
+
+int main(void)
+{
+	__disable_irq();
+
+	__enable_irq();
+
+	while (1)
+	{
+
+	}
+}
