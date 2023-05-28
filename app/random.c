76d406784a4a35d8b117ee5aa410a60dbf88e7a9
Vlad <tularodin@gmail.com>
2023-05-28 18:28:36 +0300

    random color generator

---
+++ app/random.c
@@ -0,0 +1,16 @@
+#include "random.h"
+#include "palette.h"
+
+static TIMER_TypeDef *const RND_TIMER = TIMER5;
+
+void rnd_init()
+{
+	RCU->APB1EN    |= RCU_APB1EN_TIMER5EN;
+	RND_TIMER->CAR  = sizeof(PALETTE)/sizeof(PALETTE[0]) - 1;
+	RND_TIMER->CTL0 = TIMER_CTL0_CEN;
+}
+
+uint32_t rnd_color()
+{
+	return PALETTE[RND_TIMER->CNT];
+}
---
+++ app/random.h
@@ -0,0 +1,9 @@
+#ifndef RANDOM_H_
+#define RANDOM_H_
+
+#include "libraries.h"
+
+void rnd_init();
+uint32_t rnd_color();
+
+#endif // RANDOM_H_
